5bc372a086cb72d5bee37b6b7cb0847e1363e96a
Nahidul Arafat <nahidularaf@gmail.com>
2026-07-01 17:26:32 +0600

    environment added

--- chat/config.py
+++ chat/config.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_FROM_NUMBER = os.getenv("TWILIO_FROM_NUMBER")
